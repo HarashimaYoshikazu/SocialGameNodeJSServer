ade3d428dd4a1d51e533ec240d063809a99df845
HarashimaYoshikazu <harashima318@gmail.com>
2023-07-29 16:47:35 +0900

    エラー解消

--- ts/server/ranking.ts
+++ ts/server/ranking.ts
@@ -29,11 +29,20 @@ export async function leaderboard(req: any,res: any,route: any)
 //ユーザーの現在のランキング情報
 export async function userInfo(req: any,res: any,route: any)
 {
+	console.log("ユーザーインフォ取得開始");
 	//ユーザ情報はsessionの中に全部入ってる
 	let session = await getCache(route.query.session);
 	if(!session)
 	{
+		console.log("err");
 	  return { status: 200 };
+	}
+	console.log("セッションはOK");
+	let user = await query("SELECT * FROM RankingUser WHERE userid = ?",[session.userId]);
+	if(!user)
+	{
+		console.log("ユーザーが見つからなかったから作る");
+		await query("INSERT INTO RankingUser(userid, point) VALUES ( ?, 0)",[session.userId]);
 	}
 
 	//ランキングテーブルから接続中のプレイヤーの情報を検索する
@@ -42,8 +51,10 @@ export async function userInfo(req: any,res: any,route: any)
 	
 	if(!point)
 	{
+		console.log("ユーザーIDがないよ");
 		return { status: 200 };
 	}
+
 	return { 
 		status: 200,
 		point: point,
